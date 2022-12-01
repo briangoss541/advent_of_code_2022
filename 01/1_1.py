8714594e21fbca663eac32ac927db904955fbf86
briangoss541 <35881754+briangoss541@users.noreply.github.com>
2022-12-01 09:13:23 -0500

    Initial commit

---
+++ 01/1_1.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+test_string = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+with open('input.txt') as in_file:
+    input_string = in_file.read()
+
+elf_num = 0
+elf_dict = defaultdict(int)
+for line in input_string.split('\n'):
+
+    if line == '':
+        elf_num += 1
+    else:
+        calories = int(line)
+        elf_dict[elf_num] += calories
+
+print(max(elf_dict.values()))
